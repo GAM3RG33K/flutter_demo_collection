fce9d0df8faa8fdd8ecaaf2248b77a9e040fe396
Harshvardhan Joshi <hj2931996@gmail.com>
2019-06-25 07:28:49 +0530

    changes:  - remove simple animation project  - update flutter_share_plugin dependecy form local to public

--- simple_animation_demo/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:animation_demo/src/app.dart';
-import 'package:flutter/material.dart';
-//import 'package:flutter/rendering.dart';
-
-void main() {
-//  debugPaintSizeEnabled = true;
-  runApp(App());}
